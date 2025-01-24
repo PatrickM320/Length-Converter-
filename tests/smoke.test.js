cf045b73bcddd0cd884d7f6c86d6ecef6a5ba8f5
PatrickM320 <mbousia33@gmail.com>
2025-01-24 11:56:29 +0000

    smoke.test.js

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -5,7 +5,7 @@ describe('Smoke Test for Length Converter', () => {
     test('Should load homepage successfully', (done) => {
         // GET request to site
         https.get('https://github.com/PatrickM320/Length-Converter-', (response) => {
-            // Checking if HTTP code is 200 (this means request has succeeded)
+            // Checking if HTTP code is 200- to see if it has worked
             expect(response.statusCode).toBe(200);
             done();
         }).on('error', (e) => {
